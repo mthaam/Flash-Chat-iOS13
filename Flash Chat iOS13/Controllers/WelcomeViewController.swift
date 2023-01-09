cc42d58968c74c378aa57229a0d36ff9d3bf6bd8
mthaam <79216305+mthaam@users.noreply.github.com>
2023-01-09 22:03:24 +0100

    installed pod CLTyping

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,9 +14,40 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        fibonacci(n: 10)
+        animateTitle()
     }
     
 
+}
+
+extension WelcomeViewController {
+    
+    func animateTitle() {
+        titleLabel.text = ""
+        let titleText = "⚡️FlashChat"
+        var characterIndex = 0.0
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * characterIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(letter)
+            }
+            characterIndex += 1
+        }
+
+    }
+    
+    func fibonacci(n: Int) {
+        
+        var sequence = [0, 1]
+        let numberOfSequenceToRun = n - 2
+        
+        while sequence.count < n {
+            if let lastNumber = sequence.last {
+                let previousNumber = sequence[sequence.count - 2]
+                let numberToAdd = lastNumber + previousNumber
+                sequence.append(numberToAdd)
+            }
+        }
+        print(sequence)
+    }
 }
